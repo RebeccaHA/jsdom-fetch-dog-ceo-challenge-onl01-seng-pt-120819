2864abe3b42313f28de29e50d3454b8c142e77dd
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-24 00:10:01 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 document.addEventListener('DOMContentLoaded', function(){
     fetchImages()
+    addImage()
+    fetchBreeds()
+    addBreeds()
 })
 
 function fetchImages(){
-    return fetch('https://dog.ceo/api/breeds/image/random/4')
+   fetch('https://dog.ceo/api/breeds/image/random/4')
     .then(resp => resp.json())
-    .then(json => renderImages(json))
+    .then(json =>json.message.forEach(function(image){
+        addImage(image)
+    }));
 }
 
-function renderImages(json){
+function addImage(image){
+    let element = document.createElement('img');
+    element.src = image;
+    document.getElementById('dog-image-container').appendChild(element);
+}
+
+function fetchBreeds(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(json => addBreeds(json.message)
+    )}
+ 
+
+function addBreeds(breed_hash){
+    let list = document.querySelector('#dog-breeds');
+    for(breed in breed_hash){
+    const listItem = document.createElement('li');
+    listItem.innerHTML = breed
+    list.appendChild(listItem);
+    listItem.addEventListener("click", updateTextColor )
+}}
+
+function updateTextColor(e){
+    e.target.style.cssText = 'color:blue;'
+}
+
+function filterBreeds(){
     
 }
