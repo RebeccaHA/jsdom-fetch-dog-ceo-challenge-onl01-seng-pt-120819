f41bfbdf92be3b46ed621602178fd2b30b57a90d
RebeccaHA <rebeccahunt034@gmail.com>
2020-06-23 17:36:24 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
+    fetchImages()
+})
+
+function fetchImages(){
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(json => renderImages(json))
+}
+
+function renderImages(json){
+    
+}
